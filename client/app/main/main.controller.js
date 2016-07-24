dcf838351b272a41492e62d7c38b111aa97ec464
Felipe Cardozo <dafelcardozo@gmail.com>
2016-07-24 00:29:00 -0500

    Dialog shows up.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,13 +1,18 @@
 'use strict';
 
+
+
+
 (function() {
 
+
+
   class MainController {
-    constructor($http, $scope, socket) {
+    constructor($http, $scope, socket, Auth, $mdDialog) {
       this.$http = $http;
       this.socket = socket;
-//      this.awesomeThings = [];
-
+      this.getCurrentUser = Auth.getCurrentUser;
+      this.$mdDialog = $mdDialog;
 
       $scope.$on('$destroy', function() {
         socket.unsyncUpdates('item');
@@ -15,15 +20,62 @@
     }
 
     $onInit() {
-      this.$http.get('/api/items')
-      .then(response => {
-        this.items = response.data.map(item => {
-          item.converted = item.image ?atob(btoa(String.fromCharCode.apply(null, item.image.data))):null;
-          return item;
-        });
-      });
+      this.getCurrentUser(u => 
+        this.$http.get('/api/users/'+u._id+'/inventory')
+          .then(response => this.items = response.data))
+      ;
     }
+
+    hello() {
+      alert("hello");
+    }
+
+
+  startAuction(ev, item) {
+    var useFullScreen = true;
+
+    function DialogController($scope, $mdDialog) {
+      $scope.item = item;
+      $scope.quantity;
+      $scope.minimumBid;
+
+      $scope.hide = function() {
+        console.info("Here and there");
+        $mdDialog.hide();
+      };
+      $scope.cancel = function() {
+        console.info("Here and there");
+        $mdDialog.cancel();
+      };
+      $scope.answer = function(answer) {
+        $mdDialog.hide(answer);
+      };
+      $scope.start = function() {
+        console.info("Start auction");
+      };
+    }
+
+
+    this.$mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'dialog.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: useFullScreen
+    })
+    .then(function(answer) {
+      // $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      // $scope.status = 'You cancelled the dialog.';
+    });
+    // // $scope.$watch(function() {
+    // //   return $mdMedia('xs') || $mdMedia('sm');
+    // // }, function(wantsFullScreen) {
+    // //   $scope.customFullscreen = (wantsFullScreen === true);
+    // // });
   }
+}
 
   angular.module('auctionGameApp')
     .component('main', {
@@ -31,6 +83,5 @@
       controller: MainController
     });
 })();
-
 
 
